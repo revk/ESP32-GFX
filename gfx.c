dd20712d9aeb7daa96da757ecd17d2fd4b661726
RevK <github@k.gg>
2023-07-11 16:19:27 +0100

    Border

--- gfx.c
+++ gfx.c
@@ -233,10 +233,18 @@ static void gfx_busy_wait (const char *);
 static esp_err_t gfx_send_command (uint8_t cmd);
 static esp_err_t gfx_send_gfx (void);
 static esp_err_t gfx_command (uint8_t c, const uint8_t * buf, uint16_t len);
-static __attribute__((unused)) esp_err_t gfx_command1 (uint8_t cmd, uint8_t a);
-static __attribute__((unused)) esp_err_t gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
-static __attribute__((unused)) esp_err_t gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
-static __attribute__((unused)) esp_err_t gfx_command_list (const uint8_t * init_code);
+static __attribute__((unused))
+     esp_err_t
+     gfx_command1 (uint8_t cmd, uint8_t a);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command_list (const uint8_t * init_code);
 
 // Driver (and defaults for driver)
 #ifdef  CONFIG_GFX_BUILD_SUFFIX_SSD1351
@@ -306,7 +314,8 @@ static __attribute__((unused)) esp_err_t gfx_command_list (const uint8_t * init_
 #endif
 #endif
 
-static uint8_t const sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
+     static uint8_t const
+     sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
 static uint8_t const *sevenseg[] = {
 #ifdef	CONFIG_GFX_7SEG
@@ -977,6 +986,8 @@ gfx_text (int8_t size, const char *fmt, ...)
    gfx_draw (w, h, size ? : 1, size ? : 1, &x, &y);     // starting point
    if (!w)
       return;                   // nothing to print
+#if     GFX_BPP > 1
+   // Border
    for (gfx_pos_t n = -1; n <= w; n++)
    {
       gfx_pixel (x + n, y - 1, 0);
@@ -987,6 +998,7 @@ gfx_text (int8_t size, const char *fmt, ...)
       gfx_pixel (x - 1, y + n, 0);
       gfx_pixel (x + w, y + n, 0);
    }
+#endif
    for (char *p = temp; *p; p++)
    {
       int c = *p;
