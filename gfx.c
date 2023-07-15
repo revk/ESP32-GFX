7e9e7844849a55523f6235932732ef3c1fc2862b
RevK <github@k.gg>
2023-07-15 09:57:55 +0100

    icon2 blank non draw

--- gfx.c
+++ gfx.c
@@ -233,18 +233,10 @@ static void gfx_busy_wait (const char *);
 static esp_err_t gfx_send_command (uint8_t cmd);
 static esp_err_t gfx_send_gfx (void);
 static esp_err_t gfx_command (uint8_t c, const uint8_t * buf, uint16_t len);
-static __attribute__((unused))
-     esp_err_t
-     gfx_command1 (uint8_t cmd, uint8_t a);
-     static __attribute__((unused))
-     esp_err_t
-     gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
-     static __attribute__((unused))
-     esp_err_t
-     gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
-     static __attribute__((unused))
-     esp_err_t
-     gfx_command_list (const uint8_t * init_code);
+static __attribute__((unused)) esp_err_t gfx_command1 (uint8_t cmd, uint8_t a);
+static __attribute__((unused)) esp_err_t gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
+static __attribute__((unused)) esp_err_t gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
+static __attribute__((unused)) esp_err_t gfx_command_list (const uint8_t * init_code);
 
 // Driver (and defaults for driver)
 #ifdef  CONFIG_GFX_BUILD_SUFFIX_SSD1351
@@ -314,8 +306,7 @@ static __attribute__((unused))
 #endif
 #endif
 
-     static uint8_t const
-     sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
+static uint8_t const sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
 static uint8_t const *sevenseg[] = {
 #ifdef	CONFIG_GFX_7SEG
@@ -845,15 +836,11 @@ gfx_fill (gfx_pos_t w, gfx_pos_t h, gfx_intensity_t i)
 void
 gfx_icon2 (gfx_pos_t w, gfx_pos_t h, const void *data)
 {                               // Icon, 2 bit packed
-   if (!data)
-      gfx_fill (w, h, 0);       // No icon
-   else
-   {
-      gfx_pos_t x,
-        y;
-      gfx_draw (w, h, 0, 0, &x, &y);
+   gfx_pos_t x,
+     y;
+   gfx_draw (w, h, 0, 0, &x, &y);
+   if (data)
       gfx_mask (x, y, w, h, 0, data, 0, bw);
-   }
 }
 
 void
