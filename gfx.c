d5a185767bb604792fe30d16c7c0008bb56c3422
RevK <github@k.gg>
2023-12-23 10:36:00 +0000

    Lower case

--- gfx.c
+++ gfx.c
@@ -194,10 +194,18 @@ static esp_err_t gfx_send_command (uint8_t cmd);
 static esp_err_t gfx_send_gfx (uint8_t);
 static esp_err_t gfx_send_data (const void *data, uint32_t len);
 static esp_err_t gfx_command (uint8_t c, const uint8_t * buf, uint8_t len);
-static __attribute__((unused)) esp_err_t gfx_command1 (uint8_t cmd, uint8_t a);
-static __attribute__((unused)) esp_err_t gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
-static __attribute__((unused)) esp_err_t gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
-static __attribute__((unused)) esp_err_t gfx_command_bulk (const uint8_t * init_code);
+static __attribute__((unused))
+     esp_err_t
+     gfx_command1 (uint8_t cmd, uint8_t a);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command2 (uint8_t cmd, uint8_t a, uint8_t b);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command4 (uint8_t cmd, uint8_t a, uint8_t b, uint8_t c, uint8_t d);
+     static __attribute__((unused))
+     esp_err_t
+     gfx_command_bulk (const uint8_t * init_code);
 
 // Driver (and defaults for driver)
 #ifdef  CONFIG_GFX_BUILD_SUFFIX_SSD1351
@@ -283,7 +291,8 @@ static __attribute__((unused)) esp_err_t gfx_command_bulk (const uint8_t * init_
 #endif
 #endif
 
-static uint8_t const sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
+     static uint8_t const
+     sevensegmap[] = { 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F };
 
 static uint8_t const *sevenseg[] = {
 #ifdef	CONFIG_GFX_7SEG
@@ -1376,15 +1385,18 @@ gfx_message (const char *m)
       return;
    gfx_lock ();
    gfx_pos (gfx_width () / 2, 0, GFX_T | GFX_C | GFX_V);
-   uint8_t size = (gfx_width () > 256 ? 6 : 2);
+   int8_t size = (gfx_width () > 256 ? 6 : 2);
    while (*m)
    {
       if (*m == '[')
       {
-         char isf = 1;
+         int8_t isf = 1;
+         int8_t lc = 1;
          for (; *m && *m != ']'; m++)
-            if (isdigit ((unsigned char) *m))
-               size = *m - '0'; /* size */
+            if (*m == '-')
+               lc = -lc;
+            else if (isdigit ((unsigned char) *m))
+               size = lc * (*m - '0');  /* size */
             else if (isalpha ((unsigned char) *m))
             {                   /* colour */
                if ((isf++) & 1)
