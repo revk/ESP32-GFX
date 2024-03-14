29e21708530d2dd129518aa4c2887cfabb04f6ae
RevK <github@k.gg>
2024-03-14 11:05:56 +0000

    Test

--- gfx.c
+++ gfx.c
@@ -33,7 +33,7 @@ static __attribute__((unused))
 #include "freertos/task.h"
 #include "gfx.h"
 
-#define	SPI_MAX	32768
+#define	SPI_MAX	16384
 
 #ifdef	CONFIG_GFX_BUILD_SUFFIX_GFXNONE
      const char *gfx_init_opts (gfx_init_t o)
@@ -558,7 +558,7 @@ gfx_send_data (const void *data, uint32_t len)
       esp_err_t e = spi_device_transmit (gfx_spi, &c);
       if (e)
       {
-         ESP_LOGE (TAG, "Failed send data");
+         ESP_LOGE (TAG, "Failed send data (%d)",l);
          return e;
       }
       len -= l;
