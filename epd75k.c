b5918300a4b08c1b66aad43d27468f47614c81df
RevK <github@k.gg>
2025-02-27 13:38:16 +0000

    Test

--- epd75k.c
+++ epd75k.c
@@ -87,10 +87,6 @@ gfx_driver_init (void)
       5, EPD75_PWR, 0x17, 0x17, 0x3F, 0x3F,     // 4 not 5 as no red
       2, EPD75_VDCS, 0x26,      //
       2, EPD75_PFS, 0x30,       // Power off sequence
-#ifndef	USE_AUTO
-      1, EPD75_PON,             //
-      0xFF,                     // busy wait
-#endif
       3, EPD75_CDI, 0xBB, 0x08, //
 #ifndef	FAST
       2, EPD75_PSR, 0x1F,       // KW LUT=OTP (slow update for first display)
