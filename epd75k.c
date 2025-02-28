fbd9fe32074bd82c200cbf5a308ca427268689fd
RevK <github@k.gg>
2025-02-28 11:02:07 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -80,7 +80,7 @@ gfx_driver_init (void)
    int W = gfx_settings.width;  // Must be multiple of 8
    int H = gfx_settings.height;
    const uint8_t init[] = {
-      5, EPD75_PWR, 0x17, 0x07, 0x3F, 0x3F,     // 4 not 5 as no red (second is 0x07 for slow VCOM slew)
+      5, EPD75_PWR, 0x17, 0x17, 0x3F, 0x3F,     // 4 not 5 as no red
       2, EPD75_VDCS, 0x26,      //
       2, EPD75_PFS, 0x30,       // Power off sequence
       3, EPD75_CDI, 0xBB, 0x08, //
