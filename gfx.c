129afa39a5b362a0423d5585f090e87d5603b2a5
RevK <github@k.gg>
2025-03-15 15:20:47 +0000

    testing

--- gfx.c
+++ gfx.c
@@ -1352,7 +1352,6 @@ gfx_text_draw_size (int8_t size, uint8_t z, const char *text, gfx_pos_t * wp, gf
 void
 gfx_vector_draw (int8_t size, uint8_t z, uint8_t blocky, const char *text)
 {
-	ESP_LOGE(TAG,"GFX vector %s",text);
    if (!gfx)
       return;
 
