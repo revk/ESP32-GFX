69e4f76f29434aea693c651972b07dfee43d6fc8
RevK <github@k.gg>
2025-03-15 09:10:59 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -240,8 +240,7 @@ gfx_driver_send (void)
 
    if (gfx_send_command (EPD75_DTM2))
       return "DTM2 failed";
-        if(gfx_send_data (gfx_raw_b(), GFX_PAGE))
-   //if (gfx_send_gfx (0))
+   if (gfx_send_gfx (0))
       return "Data send failed";
 
    gfx_command(EPD75_PTOUT);	// Should not be needed unless we do partial updates
