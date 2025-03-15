994e726663be2191b2a6c1f72da4a5254a28f4eb
RevK <github@k.gg>
2025-03-15 15:55:29 +0000

    testing

--- gfx.c
+++ gfx.c
@@ -1367,8 +1367,8 @@ gfx_vector_draw (int8_t size, uint8_t z, uint8_t blocky, const char *text)
    gfx_draw (w, h, 1, 1, &ox, &oy);     // starting point
    x = y = 0;
    int s0 = size / 2,
+      s1 = (size + 1) / 2,
       s2 = s1 * s1 ? : 1,
-      s1 = (size + 1) / 2;
    ox += s0;
    oy += s0;
    const char *p = text;
