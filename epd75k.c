c021776ae1da6091aa30ca3f861d1faff32f3a20
RevK <github@k.gg>
2025-03-17 13:12:53 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -99,7 +99,7 @@ gfx_driver_init (void)
       2, EPD75_VDCS, 0x26,      //
       //2, EPD75_CCSET, 0x02,   // esphome did this
       //2, EPD75_TSSET, 0x5A,   // Fixed temp?
-      2, EPD75_TSE, 0x07,       // Temp sensor internal
+      2, EPD75_TSE, 0x08,       // Temp sensor internal
       //3, EPD75_TSC, 0x00, 0x00, // Temp cal
 #ifdef	USE_AUTO
       2, EPD75_PFS, 0x30,       // Power off sequence
