0892531867020d70f82fdf3233d49f3e55334a7c
RevK <github@k.gg>
2025-03-17 13:51:37 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -97,7 +97,7 @@ gfx_driver_init (void)
       2, EPD75_DSPI, 0x00,      //
       2, EPD75_TCON, 0x22,      //
       2, EPD75_VDCS, 0x26,      //
-      2, EPD75_TSE, 0x0C,       // Temp sensor internal, offset -8
+      2, EPD75_TSE, 0x88,       // Temp sensor internal, offset -8
       //2, EPD75_TSSET, 0x5A,   // Fixed temp? datasheet not helpful
 #ifdef	USE_AUTO
       2, EPD75_PFS, 0x30,       // Power off sequence
