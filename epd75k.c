855414de41e1935c761543af75c1ac4abd37736c
RevK <github@k.gg>
2025-03-21 07:32:19 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -268,7 +268,7 @@ gfx_driver_init (void)
       3, EPD75_CDI, 0x10, 0x00, //
       2, EPD75_TCON, 0x22,      //
       5, EPD75_GSST, 0, 0, 0, 0,        // waveshare and esphome send this
-      2, EPD75_TSE, 0x08,       // Temp sensor internal, offset -8
+      //2, EPD75_TSE, 0x08,       // Temp sensor internal, offset -8
 #ifdef	USE_AUTO
       2, EPD75_PFS, 0x30,       // Power off sequence
 #endif
