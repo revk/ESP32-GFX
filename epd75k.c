593f83e57ed737ef842a947c770bb281223f4dc2
RevK <github@k.gg>
2025-03-21 07:38:35 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -256,7 +256,7 @@ gfx_driver_init (void)
 #else
       // My settings
       2, EPD75_PSR, 0,          // Reset
-      5, EPD75_PWR, 0x17, 0x17, 0x3F, 0x3F, // or 3A
+      5, EPD75_PWR, 0x17, 0x17, 0x3A, 0x3A,
       2, EPD75_VDCS, 0x26,      //
       5, EPD75_BTST, 0x17, 0x17, 0x27, 0x17,
       2, EPD75_PLL, 0x06,       //
