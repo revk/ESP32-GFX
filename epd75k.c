69b69f1f52c16674875dba6cc075d9d38d3eb973
RevK <github@k.gg>
2025-03-21 09:20:37 +0000

    testing

--- epd75k.c
+++ epd75k.c
@@ -265,14 +265,15 @@ gfx_driver_init (void)
       2, EPD75_TCON, 0x22,      //
       5, EPD75_GSST, 0, 0, 0, 0,        // waveshare and esphome send this
       //2, EPD75_TSE, 0x08,       // Temp sensor internal, offset -8
+      2, EPD75_TSE, 0x00,       // Temp sensor internal
 #ifdef	USE_AUTO
-      //2, EPD75_PFS, 0x30,       // Power off sequence
+      2, EPD75_PFS, 0x30,       // Power off sequence
 #endif
 #ifndef	CONFIG_GFX_USE_DEEP_SLEEP
       2, EPD75_AMV, 0x11,       // VCOM
 #endif
-      //2, EPD75_EVS, 0x08,       // 0x02 DC 0x08 floating
-      2, EPD75_EVS, 0x02,       // 0x02 DC 0x08 floating
+      //2, EPD75_EVS, 0x08,       // 0x08 floating
+      2, EPD75_EVS, 0x02,       // 0x02 DC
       2, EPD75_POF, 0x00,
 #endif
       0
