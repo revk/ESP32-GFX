e325e0ee0b96edd1980a18a64018176bff1c48ca
RevK <github@k.gg>
2025-04-18 07:58:18 +0100

    tweak

--- gfx.c
+++ gfx.c
@@ -534,8 +534,9 @@ gfx_pixel_argb (gfx_pos_t x, gfx_pos_t y, gfx_colour_t c)
    if (a < 255)
    {
       uint8_t was = gfx[line * y + x];
-   K = ((K * a) + (was * (255 - a)) / 255;}
-        gfx[line * y + x] = K;
+      K = ((K * a) + (was * (255 - a)) / 255;
+           }
+           gfx[line * y + x] = K;
 #elif	GFX_BPP == 16
    if (!a)
       return;                   // Do not plot
@@ -2039,13 +2040,13 @@ gfx_line2 (gfx_pos_t x1, gfx_pos_t y1, gfx_pos_t x2, gfx_pos_t y2, gfx_pos_t s)
             if (yy < dy)
                l = -icircle (yy, s);
             else if (yy <= h + dy)
-               l = w * (yy - dy) / h - dx;
+               l = (w * (yy - dy) + h / 2) / h - dx;
             else
                l = w - icircle (yy - h, s);
             if (yy < -dy)
                r = icircle (yy, s);
             else if (yy <= h - dy)
-               r = w * (yy + dy) / h + dx;
+               r = (w * (yy + dy) + h / 2) / h + dx;
             else
                r = w + icircle (yy - h, s);
             if (x1 > x2)
